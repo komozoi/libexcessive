452f8b7c0568e9b8b1f0a07c7017db21d771248a
nuclaer <dylanbrophy@protonmail.com>
2026-03-05 02:50:56 -0600

    smart pointer with COW seems to work, going to fix a test or two

--- include/alloc/pointer.h
+++ include/alloc/pointer.h
@@ -42,75 +42,179 @@ struct sp_pointer_details_t {
 template<class T>
 class sp {
 public:
-	sp() : data(nullptr) {}
+	sp() : details(nullptr) {}
+	sp(std::nullptr_t) : details(nullptr) {}
 
-	explicit sp(T* data) : data(data) {}
+	template<typename... Args>
+	explicit sp(SpPointerType type, Args&&... args) {
+		size_t total = sizeof(sp_pointer_details_t) + sizeof(T);
+		details = (sp_pointer_details_t*)::operator new(total);
 
-	sp(std::nullptr_t) : data(nullptr) {}
+		details->refs.store(1, std::memory_order_relaxed);
+		details->type = type;
 
-	sp& operator=(std::nullptr_t) {
-		reset();
-		return* this;
+		new (details->getPtr()) T(std::forward<Args>(args)...);
 	}
 
-	sp(sp&& moving) noexcept: data(nullptr) {
-		moving.swap(*this);
-		// TODO: In the comments it was pointed out that this
-		// does not match the implementation of std::unique_ptr
-		// I am going to leave mine the same. But
-		// the the standard provides some extra guarantees
-		// and probably a more intuitive usage.
-	}
+	template<typename... Args>
+	static sp<T> create(Args&&... args) {
+		size_t total = sizeof(sp_pointer_details_t) + sizeof(T);
+		auto* block = (sp_pointer_details_t*)::operator new(total);
 
-	sp& operator=(sp&& moving) noexcept {
-		moving.swap(*this);
-		return* this;
+		block->refs.store(1, std::memory_order_relaxed);
+		block->type = SpPointerType::UNIQUE;
+
+		new (block->getPtr()) T(std::forward<Args>(args)...);
+
+		sp p;
+		p.details = block;
+		return p;
 	}
 
 	template<typename U>
-	sp(sp<U>&& moving) {
-		sp<T> tmp(moving.release());
-		tmp.swap(*this);
+	explicit sp(U&& value) {
+		size_t total = sizeof(sp_pointer_details_t) + sizeof(T);
+		details = (sp_pointer_details_t*)::operator new(total);
+
+		details->refs.store(1, std::memory_order_relaxed);
+		details->type = SpPointerType::UNIQUE;
+
+		new (details->getPtr()) T(std::forward<U>(value));
 	}
 
-	template<typename U>
-	sp& operator=(sp<U>&& moving) {
-		sp<T> tmp(moving.release());
-		tmp.swap(*this);
-		return* this;
+	explicit sp(const T& value) {
+		size_t total = sizeof(sp_pointer_details_t) + sizeof(T);
+		details = (sp_pointer_details_t*)::operator new(total);
+
+		details->refs.store(1, std::memory_order_relaxed);
+		details->type = SpPointerType::UNIQUE;
+
+		new (details->getPtr()) T(value);
 	}
 
-	sp(sp const& ) = delete;
-	sp& operator=(sp const& ) = delete;
-
-	T* operator->() const { return data; }
-	T& operator*() const { return* data; }
-
-	T* get() const { return data; }
-
-	explicit operator bool() const { return data; }
-
-	T* release() noexcept {
-		T* result = nullptr;
-		std::swap(result, data);
-		return result;
+	// Move
+	sp(sp&& other) noexcept : details(nullptr) {
+		swap(other);
 	}
 
-	void swap(sp& src) noexcept {
-		std::swap(data, src.data);
+	sp& operator=(sp&& other) noexcept {
+		swap(other);
+		return *this;
 	}
 
-	void reset() {
-		T* tmp = release();
-		delete tmp;
+	// Copy (enables COW logic)
+	sp(sp const& other) {
+		acquire_from_copy(other);
+	}
+
+	sp& operator=(sp const& other) {
+		if (this != &other) {
+			reset();
+			acquire_from_copy(other);
+		}
+		return *this;
 	}
 
 	~sp() {
-		delete data;
+		release_ref();
+	}
+
+	// ---------- Access ----------
+
+	const T* operator->() const {
+		return get();
+	}
+
+	const T& operator*() const {
+		return *get();
+	}
+
+	T& mut() {
+		detach_if_needed();
+		return *get();
+	}
+
+	T* get() const {
+		return details ? (T*)details->getPtr() : nullptr;
+	}
+
+	explicit operator bool() const {
+		return details != nullptr;
+	}
+
+	// ---------- Modifiers ----------
+
+	void reset() {
+		release_ref();
+		details = nullptr;
+	}
+
+	void swap(sp& other) noexcept {
+		std::swap(details, other.details);
 	}
 
 private:
 	sp_pointer_details_t* details;
+
+	// ---------- Internals ----------
+
+	void acquire_from_copy(sp const& other) {
+		details = other.details;
+		if (!details)
+			return;
+
+		if (details->type == SpPointerType::UNIQUE) {
+			// UNIQUE copied -> becomes COW
+			details->type = SpPointerType::COPY_ON_WRITE;
+		}
+
+		details->refs.fetch_add(1, std::memory_order_relaxed);
+	}
+
+	void release_ref() {
+		if (!details)
+			return;
+
+		if (details->refs.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+			// destroy object
+			T* obj = (T*)details->getPtr();
+			obj->~T();
+			::operator delete(details);
+		}
+
+		details = nullptr;
+	}
+
+	void detach_if_needed() {
+		if (!details)
+			return;
+
+		if (details->type != SpPointerType::COPY_ON_WRITE)
+			return;
+
+		if (details->refs.load(std::memory_order_acquire) == 1)
+			return;
+
+		// Need deep copy
+		sp_pointer_details_t* old = details;
+
+		size_t total = sizeof(sp_pointer_details_t) + sizeof(T);
+		auto* block = (sp_pointer_details_t*)::operator new(total);
+
+		block->refs.store(1, std::memory_order_relaxed);
+		block->type = SpPointerType::COPY_ON_WRITE;
+
+		new (block->getPtr()) T(*(T*)old->getPtr());
+
+		details = block;
+
+		// drop old ref
+		if (old->refs.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+			T* obj = (T*)old->getPtr();
+			obj->~T();
+			::operator delete(old);
+		}
+	}
 };
 
 template<typename T>
