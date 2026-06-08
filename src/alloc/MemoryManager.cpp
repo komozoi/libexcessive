995215269886060f114a8eda6d0b530e7b5905a9
komozoi <komozoi@protonmail.com>
2026-06-07 21:06:29 -0400

    Update checkFreeSystemMemoryBytes to be compatible with macOS

--- src/alloc/MemoryManager.cpp
+++ src/alloc/MemoryManager.cpp
@@ -17,13 +17,27 @@
 
 
 #include "alloc/MemoryManager.h"
+#ifdef __APPLE__
+#include <mach/mach.h>
+#include <unistd.h>
+#else
 #include <sys/sysinfo.h>
+#endif
 
 
 size_t checkFreeSystemMemoryBytes() {
+#ifdef __APPLE__
+	mach_msg_type_number_t count = HOST_VM_INFO64_COUNT;
+	vm_statistics64_data_t vm_stats;
+	if (host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vm_stats, &count) == KERN_SUCCESS) {
+		return (size_t)vm_stats.free_count * sysconf(_SC_PAGESIZE);
+	}
+	return 0;
+#else
 	struct sysinfo info;
 	sysinfo(&info);
-	return info.freeram * info.mem_unit;
+	return (size_t)info.freeram * info.mem_unit;
+#endif
 }
 
 
